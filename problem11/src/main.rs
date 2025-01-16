43e508e16a37e6f6f5f11e522ae63daf441ae57b
MarcoTz <marco.tzschentke@gmail.com>
2025-01-16 19:12:31 +0100

    updated problem 11

--- problem11/src/main.rs
+++ problem11/src/main.rs
@@ -1,94 +1,116 @@
-use std::fs::read_to_string;
+use std::{fmt, fs::read_to_string};
 
 const GRID_FILENAME: &str = "grid.txt";
 const GRID_WIDTH: usize = 20;
 const GRID_HEIGHT: usize = 20;
 
-pub type Grid = [[i32; GRID_WIDTH]; GRID_HEIGHT];
-
-fn load_grid() -> Result<Grid, String> {
-    let mut grid = [[0; GRID_WIDTH]; GRID_HEIGHT];
-    let contents = read_to_string(GRID_FILENAME).map_err(|err| err.to_string())?;
-    for (line_num, line) in contents.lines().enumerate() {
-        let nums = line
-            .split(' ')
-            .map(|num| num.trim().parse::<i32>().map_err(|err| err.to_string()))
-            .collect::<Result<Vec<i32>, String>>()
-            .map_err(|err| err.to_string())?;
-        for (num_num, num) in nums.iter().enumerate() {
-            grid[line_num][num_num] = *num;
-        }
-    }
-    Ok(grid)
+enum Dir {
+    Row,
+    Column,
+    Diag,
+    InvDiag,
 }
 
-fn max_prod_row(grid: &Grid, row_num: usize) -> i32 {
-    let mut prod = grid[row_num][0] * grid[row_num][1] * grid[row_num][2] * grid[row_num][3];
-    if prod == 0 {
-        return 0;
-    };
-    let mut max_prod = prod;
-    for i in 4..GRID_WIDTH {
-        if grid[row_num][i] == 0 {
-            return max_prod;
+#[derive(Copy, Clone)]
+struct FourNums {
+    a: u64,
+    b: u64,
+    c: u64,
+    d: u64,
+}
+
+impl FourNums {
+    fn prod(self) -> u64 {
+        self.a * self.b * self.c * self.d
+    }
+}
+
+struct Grid([[u64; GRID_WIDTH]; GRID_HEIGHT]);
+
+impl Grid {
+    fn load() -> Grid {
+        let mut grid = [[0; GRID_WIDTH]; GRID_HEIGHT];
+        let contents = read_to_string(GRID_FILENAME).unwrap();
+        for (line_num, line) in contents.lines().enumerate() {
+            let nums = line
+                .split(' ')
+                .map(|num| num.trim().parse::<u64>().unwrap())
+                .collect::<Vec<u64>>();
+            for (num_num, num) in nums.iter().enumerate() {
+                grid[line_num][num_num] = *num;
+            }
+        }
+        Grid(grid)
+    }
+
+    fn four_dir(&self, start_x: usize, start_y: usize, dir: Dir) -> Option<FourNums> {
+        let coords = match dir {
+            Dir::Row => (
+                (start_x, start_y),
+                (start_x + 1, start_y),
+                (start_x + 2, start_y),
+                (start_x + 3, start_y),
+            ),
+            Dir::Column => (
+                (start_x, start_y),
+                (start_x, start_y + 1),
+                (start_x, start_y + 2),
+                (start_x, start_y + 3),
+            ),
+            Dir::Diag => (
+                (start_x, start_y),
+                (start_x + 1, start_y + 1),
+                (start_x + 2, start_y + 2),
+                (start_x + 3, start_y + 3),
+            ),
+            Dir::InvDiag => {
+                if start_y < 3 {
+                    return None;
+                }
+                (
+                    (start_x, start_y),
+                    (start_x + 1, start_y - 1),
+                    (start_x + 2, start_y - 2),
+                    (start_x + 3, start_y - 3),
+                )
+            }
         };
-        prod *= grid[row_num][i] / grid[row_num][i - 4];
-        max_prod = prod.max(max_prod);
+        let a = *self.0.get(coords.0 .0)?.get(coords.0 .1)?;
+        let b = *self.0.get(coords.1 .0)?.get(coords.1 .1)?;
+        let c = *self.0.get(coords.2 .0)?.get(coords.2 .1)?;
+        let d = *self.0.get(coords.3 .0)?.get(coords.3 .1)?;
+        Some(FourNums { a, b, c, d })
     }
-    max_prod
 }
 
-fn max_prod_col(grid: &Grid, col_num: usize) -> i32 {
-    let mut prod = grid[0][col_num] * grid[1][col_num] * grid[2][col_num] * grid[3][col_num];
-    if prod == 0 {
-        return prod;
-    };
-    let mut max_prod = prod;
-    for i in 4..GRID_HEIGHT {
-        if grid[i][col_num] == 0 {
-            return max_prod;
-        };
-
-        prod *= grid[i][col_num] / grid[i - 4][col_num];
-        max_prod = prod.max(max_prod);
-    }
-    max_prod
-}
-
-fn max_prod_diag(grid: &Grid, diag_num: usize) -> i32 {
-    if diag_num >= GRID_HEIGHT - 4 {
-        return 0;
-    }
-
-    let mut prod =
-        grid[diag_num][0] * grid[diag_num + 1][1] * grid[diag_num + 2][2] * grid[diag_num + 3][3];
-    if prod == 0 {
-        return 0;
-    }
-    let mut max_prod = prod;
-    for i in 4..GRID_HEIGHT.min(GRID_WIDTH) {
-        if diag_num + i >= GRID_WIDTH - 1 {
-            return max_prod;
+impl fmt::Display for Grid {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in self.0.iter() {
+            for val in row.iter() {
+                write!(f, " {val:02} ")?;
+            }
+            writeln!(f)?;
         }
-        if grid[diag_num + i][i] == 0 {
-            return max_prod;
-        }
-
-        prod *= grid[diag_num + i][i] / grid[diag_num + i - 4][i - 4];
-        max_prod = max_prod.max(prod);
+        Ok(())
     }
-    max_prod
 }
 
-fn main() -> Result<(), String> {
-    let grid = load_grid()?;
-    let mut max_prod = 1;
-    for i in 0..GRID_WIDTH.min(GRID_HEIGHT) {
-        let max_row = max_prod_row(&grid, i);
-        let max_col = max_prod_col(&grid, i);
-        let max_diag = max_prod_diag(&grid, i);
-        max_prod = max_prod.max(max_row).max(max_col).max(max_diag);
+impl fmt::Display for FourNums {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{},{},{},{}", self.a, self.b, self.c, self.d)
     }
-    println!("{max_prod}",);
-    Ok(())
+}
+fn main() {
+    let grid = Grid::load();
+    let mut prods = vec![];
+    for x in 0..GRID_WIDTH {
+        for y in 0..GRID_HEIGHT {
+            prods.push(grid.four_dir(x, y, Dir::Row).map(FourNums::prod));
+            prods.push(grid.four_dir(x, y, Dir::Column).map(FourNums::prod));
+            prods.push(grid.four_dir(x, y, Dir::Diag).map(FourNums::prod));
+            prods.push(grid.four_dir(x, y, Dir::InvDiag).map(FourNums::prod));
+        }
+    }
+    let remaining = prods.into_iter().filter_map(|x| x);
+    println!("{}", remaining.max().unwrap())
 }
