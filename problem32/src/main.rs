ff628e16d2aa2e26b97ff610291cbccb21e6097b
MarcoTz <marco.tzschentke@gmail.com>
2025-01-17 00:17:32 +0100

    updated problem 32

--- problem32/src/main.rs
+++ problem32/src/main.rs
@@ -1,105 +1,48 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
-fn num_digit_combinations() -> HashSet<(usize, usize, usize)> {
-    let mut combinations = HashSet::new();
-    for i in 1..=9 {
-        for j in 1..=9 {
-            for k in 1..=9 {
-                if i + j + k != 9 {
-                    continue;
-                }
-                if i <= k && j <= k && k <= j + i && !combinations.contains(&(j, i, k)) {
-                    combinations.insert((i, j, k));
-                }
-                if i <= j && k <= j && j <= i + k && !combinations.contains(&(k, i, j)) {
-                    combinations.insert((i, k, j));
-                }
-                if j <= i && k <= i && i <= j + k && !combinations.contains(&(k, j, i)) {
-                    combinations.insert((j, k, i));
-                }
-            }
+fn factors(num: u64) -> Vec<(u64, u64)> {
+    let mut factors = vec![];
+    for i in 1..=(num as f64).sqrt().ceil() as u64 {
+        if num % i == 0 {
+            factors.push((i, num / i))
         }
     }
-    combinations
+    factors
 }
 
-fn nums_digits(num_digits: usize, cache: &mut HashMap<usize, Vec<usize>>) -> Vec<usize> {
-    if let Some(nums) = cache.get(&num_digits) {
-        return nums.clone();
-    }
-    let nums: Vec<usize> =
-        (10_usize.pow(num_digits as u32 - 1)..10_usize.pow(num_digits as u32)).collect();
-    cache.insert(num_digits, nums.clone());
-    nums
-}
-
-fn digits(num: usize) -> Vec<usize> {
+fn digits(num: u64) -> Vec<u8> {
     let mut num = num;
     let mut digits = vec![];
     while num >= 10 {
-        digits.push(num % 10);
+        digits.push((num % 10) as u8);
         num = num / 10;
     }
-    digits.push(num);
+    digits.push(num as u8);
     digits.reverse();
     digits
 }
 
+fn pandigital(a: u64, b: u64, c: u64) -> bool {
+    let mut digs = digits(a);
+    digs.extend(digits(b));
+    digs.extend(digits(c));
+    if digs.len() != 9 {
+        return false;
+    }
+    digs.sort();
+    digs == (1..=9).collect::<Vec<u8>>()
+}
+
 fn main() {
-    let combinations = num_digit_combinations();
-    let digit_set = HashSet::from([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    println!(
-        "got combinations\n{}",
-        combinations
-            .iter()
-            .map(|comb| format!("{},{},{}", comb.0, comb.1, comb.2))
-            .collect::<Vec<String>>()
-            .join("\n")
-    );
-
-    let mut solutions = vec![];
-    let mut cache = HashMap::new();
-    for (num_a, num_b, num_c) in combinations {
-        let candidates_a = nums_digits(num_a, &mut cache);
-        let candidates_b = nums_digits(num_b, &mut cache);
-        println!("got candidates for combination {num_a},{num_b},{num_c}");
-        for a in candidates_a.iter() {
-            for b in candidates_b.iter() {
-                let prod = a * b;
-                let prod_digits = digits(prod);
-
-                if prod_digits.len() != num_c {
-                    continue;
-                }
-                let mut all_digits = prod_digits;
-                all_digits.extend(digits(*a));
-                all_digits.extend(digits(*b));
-                if all_digits.len() != 9 {
-                    continue;
-                }
-                if digit_set != all_digits.into_iter().collect() {
-                    continue;
-                }
-                solutions.push((*a, *b, prod));
+    let mut pandigitals = vec![];
+    for c in 1..=10000 {
+        let factors = factors(c);
+        for (a, b) in factors {
+            if pandigital(a, b, c) {
+                pandigitals.push((a, b, c));
             }
         }
-        println!("finished combination {num_a},{num_b},{num_c}");
     }
-    println!(
-        "found solutions:\n{}",
-        solutions
-            .iter()
-            .map(|solution| format!("{}*{}={}", solution.0, solution.1, solution.2))
-            .collect::<Vec<String>>()
-            .join("\n")
-    );
-    let sums: Vec<usize> = solutions.iter().map(|sol| sol.0 + sol.1 + sol.2).collect();
-    println!(
-        "sums for products:\n{}",
-        sums.iter()
-            .map(|sum| sum.to_string())
-            .collect::<Vec<String>>()
-            .join("\n")
-    );
-    println!("total sum: {}", sums.iter().sum::<usize>());
+    let cs: HashSet<u64> = pandigitals.iter().map(|(_, _, c)| *c).collect();
+    println!("{}", cs.iter().sum::<u64>());
 }
